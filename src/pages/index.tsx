77a207d920eed5f75d8656561db87cb7d1e12f2b
BabyMsp2k <65636660+BabyMsp2k@users.noreply.github.com>
2025-03-02 14:57:36 +0100

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,9 +57,8 @@ export default function HomePage(): JSX.Element {
 							scale: [0.75, 1],
 						}}
 						className="text-gray-500 dark:text-white text-5xl sm:text-6xl md:text-6xl lg:text-8xl tracking-tight font-extrabold">
-						Hey <span className="inline-block origin-70 hover:(animate-wave)">👋</span>{' '}
-						I&apos;m Ben, <br className="hidden sm:block" />a{' '}
-						<Pill.Standard className="mt-4">developer</Pill.Standard>
+						Ahoj <span className="inline-block origin-70 hover:(animate-wave)">👋</span>{' '}
+						Já jsem BabyMsp2k, <Pill.Standard className="mt-4">developer</Pill.Standard>
 					</Animate>
 
 					<Animate
