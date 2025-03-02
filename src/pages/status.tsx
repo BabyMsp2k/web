8406e0b6c8146f620fc0184b3c47d92dfda87c2f
BabyMsp2k <65636660+BabyMsp2k@users.noreply.github.com>
2025-03-02 14:43:42 +0100

    status redirect

--- src/pages/status.tsx
+++ src/pages/status.tsx
@@ -1,14 +1,9 @@
-import { Layout } from '~/layouts';
-import { Status } from '~/components';
+import { useEffect } from 'react';
 
 export default function StatusPage(): JSX.Element {
-	return (
-		<Layout.Default seo={{ title: 'nuro ─ status' }}>
-			<div className="flex flex-grow min-h-screen pt-16 pb-12">
-				<div className="flex-grow flex flex-col justify-center max-w-sm sm:max-w-lg w-full mx-auto px-0 sm:px-16">
-					<Status.Widget />
-				</div>
-			</div>
-		</Layout.Default>
-	);
+	useEffect(() => {
+		window.location.href = 'https://status.babymsp2k.wtf';
+	}, []);
+
+	return null;
 }
